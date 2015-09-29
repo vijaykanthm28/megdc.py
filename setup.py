24f29e6b84deb4485cb3bdf6c140b35c22d9f2ba
indykish <nkishore@megam.io>
2015-09-29 18:21:38 +0530

    cleaned up  ~ files, and used virtualenv.

--- setup.py
+++ setup.py
@@ -1,56 +1,54 @@
 from setuptools import setup, find_packages
 import os
 import sys
-import megdc 
+import megdc
 
 
 def read(fname):
-    path = os.path.join(os.path.dirname(__file__), fname)
-    f = open(path)
-    return f.read()
+  path = os.path.join(os.path.dirname(__file__), fname)
+  f = open(path)
+  return f.read()
 
 install_requires = []
 pyversion = sys.version_info[:2]
 if pyversion < (2, 7) or (3, 0) <= pyversion <= (3, 1):
-    install_requires.append('argparse')
+  install_requires.append('argparse')
 
-#
-# Add libraries that are not part of install_requires but only if we really
-# want to, specified by the environment flag
-#
 
 setup(
     name='megdc',
     version=megdc.__version__,
     packages=find_packages(),
 
-    author='Megam system team',
-    author_email='support@megam.io',
-    description='Deploy megam with minimal infrastructure',
+    author='Megam Systems',
+    author_email='mvijaykanth@megam.io',
+    description='Deploy a hyper converged datacenter using OpenNebula, Megam, Ceph, Docker in your own infrastructure',
     long_description=read('README.rst'),
-    license='MEGAM SYSTEM',
-    keywords='megdc',
+    license='ApacheV2',
+    keywords='megam data center',
     url="https://github.com/megamsys/megdc",
 
     install_requires=[
         'setuptools',
-        ] + install_requires,
+    ] + install_requires,
 
     tests_require=[
         'pytest >=2.1.3',
         'mock >=1.0b1',
-        ],
+    ],
 
     entry_points={
 
         'console_scripts': [
-            'megdc = megdc.cib:main',
-            ],
+            'megdc = megdc.cli:main',
+        ],
 
-        'megdc.cib': [
-            'install = megdc.install:make',
-            'uninstall = megdc.install:make_uninstall',
-            ],
+        'megdc.cli': [
+            'new = megdc.new:make',
+            'megam = megdc.megam:make',
+            'one = megdc.one:make',
+            'ceph = megdc.ceph:make',
+        ],
 
-        },
-    )
+    },
+)
